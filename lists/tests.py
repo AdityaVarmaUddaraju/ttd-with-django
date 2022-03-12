2baefcbb75e975a5c06d004fcfd62c0516f9c344
AdityaVarmaUddaraju <adithya951@gmail.com>
2022-03-12 22:32:52 +0530

    redirect after post, save new item in db and display all the items in table

--- lists/tests.py
+++ lists/tests.py
@@ -14,10 +14,25 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'lists/home.html')
 
-    def test_add_input_item_to_todo_list(self):
+    def test_add_item_to_db_on_post(self):
+        self.client.post('/lists/',data={'item_text': 'a new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        item = Item.objects.first()
+        self.assertEqual(item.text, 'a new list item')
+
+    def test_redirect_after_post(self):
         response = self.client.post('/lists/',data={'item_text': 'a new list item'})
-        self.assertIn('a new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+        self.assertEqual(response.status_code, 302)
+    
+    def test_multiple_items_in_list(self):
+        Item.objects.create(text='item1')
+        Item.objects.create(text='item2')
+
+        response = self.client.get('/lists/')
+
+        self.assertIn('1. item1', response.content.decode())
+        self.assertIn('2. item2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from .models import Item
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text = request.POST['item_text'])
+        return redirect('/')
+    item_list = Item.objects.all()
     return render(request, 'lists/home.html', {
-        'new_item_text': request.POST.get('item_text','')
+        'item_list': item_list
     })
