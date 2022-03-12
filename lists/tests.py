d030fba15e8fffe442afcd3d41fefab36953d4c3
AdityaVarmaUddaraju <adithya951@gmail.com>
2022-03-12 16:46:02 +0530

    front page html now renders from template

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import resolve
 from django.http import HttpRequest
 
@@ -6,15 +6,12 @@ from .views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/lists/')
-        self.assertEqual(found.func, home_page)
+    def test_root_url_renders_home_page_template(self):
+        self.c = Client()
+        response = self.c.get('/lists/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lists/home.html')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do App</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+
+
 
